177f2a590105b641c7b3e90b7c2e81483e8947d7
Kacper <kacper.laudanski@wp.pl>
2024-03-30 19:47:45 +0100

    basic structure and styling

--- script.ts
+++ script.ts
@@ -1,32 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
-async function createUser(name: string, age: number) {
-  const newUser = await prisma.user.create({
-    data: {
-      name: name,
-      age: age,
-    },
-  });
-}
-
-async function findUserByName(name: string){
-    const user = await prisma.user.findMany({
-        where:{
-            name:name
-        }
-    })
-
-    return user; 
-}
 
 async function main() {
-  await prisma.user.deleteMany();
-  await createUser("Alex", 23);
-  await createUser("Christy", 43);
-  //const users = await prisma.user.findMany();
-  const foundUsers = await findUserByName("Alex");
-  console.log(foundUsers);
+   
 }
 
 main()
